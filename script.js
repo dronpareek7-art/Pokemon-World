2a8a900d47804d1b577d94abc59915db823f38d8
Dron Pareek <dronpareek@example.com>
2026-01-21 15:48:41 +0530

    css and js update

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let wrapper = document.querySelector(".wrapper");
+let input =document.querySelector(".input");
+let loadMore = document.querySelector(".load-more");
+
+let limit = 20;
+let offset = 0;
 
 async function getpok() {
   try {
     let response = await fetch(
-      "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0",
+      `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
     );
     let data = await response.json();
     console.log(data);
@@ -20,20 +25,46 @@ async function displaydata(data) {
       let result = await fetch(item.url);
       let details = await result.json();
       console.log(details);
+        
+       let card = document.createElement("div");
+      card.classList.add("card");
+
+      let front = document.createElement("div")
+      front.classList.add("card-front")
+
+      let back = document.createElement("div")
+      back.classList.add("card-back")
+
+      back.innerHTML = `
+      <p>Weight: ${details.weight}kg</p>
+      <p>Height: ${details.height}m</p>`
+
+      let inner = document.createElement("div")
+      inner.classList.add("card-inner")
 
       let nameofpokemon = document.createElement("p");
-      let div = document.createElement("div");
-      div.classList.add("card")
       nameofpokemon.classList.add("namepok");
       nameofpokemon.innerHTML = item.name;
+
+      let div = document.createElement("div");
+      div.classList.add("card");
+
       let image = document.createElement("img");
       image.classList.add("pok-image");
-
       image.src = details.sprites.other.dream_world.front_default;
-      div.append(image, nameofpokemon);
-      wrapper.append(div);
+       
+          front.append(image,nameofpokemon);
+          inner.append(front,back)
+          card.append(inner)
+          wrapper.append(card)
     } catch (error) {
       console.log(error);
     }
   });
 }
+
+loadMore.addEventListener("click",()=>{
+  offset += 20;
+  getpok()
+})
+
